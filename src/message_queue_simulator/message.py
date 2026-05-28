2604b86774c66289fb858c979c6f49a34fc98fbc
Matute0512 <matiast999@hotmail.com>
2026-05-28 11:11:50 -0300

    feat: create message domain model
    Se implementó la entidad Message como una dataclass
    inmutable con generación automática de UUID y fecha
    de creación.
    
    Se agregó el enum Priority para representar los
    niveles de prioridad de los mensajes.
    
    También se incorporaron pruebas unitarias para validar
    la creación de mensajes, la generación de identificadores
    únicos y la inmutabilidad de la entidad.
    
    Se configuró Pytest para trabajar correctamente con
    la estructura src/tests del proyecto.

---
+++ src/message_queue_simulator/message.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from uuid import UUID, uuid4
+
+from message_queue_simulator.priority import Priority
+
+
+@dataclass(frozen=True)
+class Message():
+    payload: dict
+    priority: Priority = Priority.MEDIUM
+    id: UUID = field(default_factory=uuid4)
+    created_at: datetime = field(default_factory=datetime.now)
---
+++ src/message_queue_simulator/priority.py
@@ -0,0 +1,7 @@
+from enum import IntEnum
+
+
+class Priority(IntEnum):
+    HIGH = 0
+    MEDIUM = 1
+    LOW = 2
