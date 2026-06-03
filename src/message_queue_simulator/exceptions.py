ddf3b1357c3369f7cd6c2dc0f29317f6979d2a4c
Matute0512 <matiast999@hotmail.com>
2026-06-02 23:26:32 -0300

    feat(queue): add max_size capability and QueueFullError.
    - Se agrego la capacidad de definir un tamaño máximo (max_size) en MessageQueue.
    - Se implementó la excepción QueueFullError cuando un Producter intenta encolar en una cola llena.
    - Se completó el ciclo TDD asegurando la calidad con Black y Ruff.

--- src/message_queue_simulator/exceptions.py
+++ src/message_queue_simulator/exceptions.py
@@ -1,2 +1,6 @@
 class QueueEmptyError(Exception):
     "Raised when attempting to dequeue from an empty queue."
+
+
+class QueueFullError(Exception):
+    "Raised when attempting to enqueue to a full queue."
